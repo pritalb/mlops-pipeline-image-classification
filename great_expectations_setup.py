eff2cc4078bed011db4f3de1f9c4d9f58faa424b
pritalb <work.prital@gmail.com>
2025-02-26 20:09:33 +0530

    Implemented GX Expectations suite and validation definitions

--- great_expectations_setup.py
+++ great_expectations_setup.py
@@ -25,5 +25,26 @@ data_source = context.data_sources.add_pandas(name="cifar10_pandas")
 data_asset = data_source.add_dataframe_asset(name="cifar10_asset")
 data_batch_definition = data_asset.add_batch_definition_whole_dataframe(name="cifar10_batch_definition")
 data_batch = data_batch_definition.get_batch(batch_parameters={"dataframe": cifar10_df})
-
 print("Successfully added data asset.")
+
+suite = gx.ExpectationSuite(name="cifar10_expectation_suite")
+
+cifar10_df.columns = cifar10_df.columns.map(str)
+for column in cifar10_df.columns:
+    suite.add_expectation(
+        gx.expectations.ExpectColumnValuesToBeInTypeList(
+            column=column,
+            type_list=["int32", "int64", "float64", "uint8", "numpy.int32", "numpy.int64", "numpy.float64", "numpy.uint8"],
+        )
+    )
+suite = context.suites.add(suite)
+print("Successfully added expectations.")
+
+validation_definition = gx.ValidationDefinition(
+    data=data_batch_definition,
+    suite=suite,
+    name="cifar10_validation_definiton",
+)
+validation_results = validation_definition.run(batch_parameters={"dataframe": cifar10_df})
+
+print(validation_results)
